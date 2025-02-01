9cb4a3c8e3a473d0fff770570a675dea0887c903
chiahsun <chiahsun0814@gmail.com>
2025-02-01 19:59:34 +0800

    Add navigation basics

--- flutter.dev/cookbook/navigation/navigation_basics/lib/main.dart
+++ flutter.dev/cookbook/navigation/navigation_basics/lib/main.dart
@@ -1,8 +1,47 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class FirstRoute extends StatelessWidget {
+  const FirstRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('First Route')),
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () {
+              Navigator.push( // <1>
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const SecondRoute()));
+            },
+            child: const Text('Open route')),
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Second Route')),
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context); // <2>
+            },
+            child: const Text('Go back!')),
+      ),
+    );
+  }
+}
 
 void main() {
-  runApp(const CupertinoApp(
+  runApp(const MaterialApp(
     title: 'Navigation Basics',
-    home: Text('MyApp'),
+    home: FirstRoute(),
   ));
 }
