611f411ccc7cc868c757191331903360ed3281cc
chiahsun <chiahsun0814@gmail.com>
2025-02-01 16:03:59 +0800

    Add named routes first and second screens

---
+++ flutter.dev/cookbook/navigation/named_routes/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('First Screen'),),
+      body: Center(child: ElevatedButton(onPressed: () { }, child: const Text('Launch screen')),),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Second Screen'),),
+      body: Center(child: ElevatedButton(onPressed: () { }, child: const Text('Go back!')),),
+    );
+  }
+}
+
+
+void main() {
+  runApp(MaterialApp(title: 'Named Routes Demo', initialRoute: '/', routes: {
+    '/': (context) => const FirstScreen(),
+    '/second': (context) => const SecondScreen(),
+  }));
+}
