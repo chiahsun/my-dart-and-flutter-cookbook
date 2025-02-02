a491d7ef8a47f36004993b32ba5a7cffe4aa28ec
chiahsun <chiahsun0814@gmail.com>
2025-02-02 15:53:34 +0800

    Add material appbar empty project

---
+++ flutter.dev/material/appbar/appbar1/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AppBarApp());
+}
+
+class AppBarApp extends StatelessWidget {
+  const AppBarApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const AppBarExample(),
+    );
+  }
+}
+
+class AppBarExample extends StatelessWidget {
+  const AppBarExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
