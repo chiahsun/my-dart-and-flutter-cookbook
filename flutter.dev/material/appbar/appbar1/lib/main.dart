56dc7216823fd6410ab05cf04d4d49e03f014135
chiahsun <chiahsun0814@gmail.com>
2025-02-02 16:08:21 +0800

    Add material appbar sample 1

--- flutter.dev/material/appbar/appbar1/lib/main.dart
+++ flutter.dev/material/appbar/appbar1/lib/main.dart
@@ -20,6 +20,40 @@ class AppBarExample extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('AppBar Demo'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.add_alert), tooltip: 'Show Snackbar', onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('This is a snackbar'))
+            );
+          },),
+          IconButton(onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) => const NextPage()));
+          }, icon: const Icon(Icons.navigate_next),
+          tooltip: 'Go to the next page',)
+        ],
+      ),
+      body: const Center(
+        child: Text('This is the home page', style: TextStyle(fontSize: 24)),
+      ),
+    );
+  }
+}
+
+class NextPage extends StatelessWidget {
+  const NextPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Next Page'),
+      ),
+      body: const Center(
+        child: Text('This is the next page', style: TextStyle(fontSize: 24)),
+      ),
+    );
   }
 }
