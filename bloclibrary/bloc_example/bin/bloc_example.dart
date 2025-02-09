7d4c995b427f0017685331b5ba9eefadd7a17d2a
chiahsun <chiahsun0814@gmail.com>
2025-02-09 23:19:29 +0800

    Add bloc example sample project

---
+++ bloclibrary/bloc_example/bin/bloc_example.dart
@@ -0,0 +1,5 @@
+import 'package:bloc_example/bloc_example.dart' as bloc_example;
+
+void main(List<String> arguments) {
+  print('Hello world: ${bloc_example.calculate()}!');
+}
---
+++ bloclibrary/bloc_example/test/bloc_example_test.dart
@@ -0,0 +1,8 @@
+import 'package:bloc_example/bloc_example.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
