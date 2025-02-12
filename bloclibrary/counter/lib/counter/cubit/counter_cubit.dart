419ec823fa96c833a2c272136232b79e98c13ea6
chiahsun <chiahsun0814@gmail.com>
2025-02-12 21:57:46 +0800

    Let counter view display a number

--- bloclibrary/counter/lib/counter/cubit/counter_cubit.dart
+++ bloclibrary/counter/lib/counter/cubit/counter_cubit.dart
@@ -1,8 +1,8 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-part 'counter_state.dart';
+class CounterCubit extends Cubit<int> {
+  CounterCubit() : super(1);
 
-class CounterCubit extends Cubit<CounterState> {
-  CounterCubit() : super(CounterInitial());
+  void increment() => emit(state + 1);
+  void decrement() => emit(state - 1);
 }
--- bloclibrary/counter/lib/counter/view/counter_view.dart
+++ bloclibrary/counter/lib/counter/view/counter_view.dart
@@ -1,10 +1,21 @@
+import 'package:counter/counter/cubit/counter_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CounterView extends StatelessWidget {
   const CounterView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final textTheme = Theme.of(context).textTheme;
+    return Scaffold(
+      body: Center(
+        child: BlocBuilder<CounterCubit, int>(
+          builder: (context, state) {
+            return Text('$state', style: textTheme.displayMedium);
+          },
+        )
+      ),
+    );
   }
 }
